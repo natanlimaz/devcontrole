a18485cfaa426109ec8264551e07ab50a32f83ab
natanlimaz <natanlimadev@gmail.com>
2024-10-09 21:12:42 -0300

    feat: Configurando página de clientes, cadastro de clientes, configurando react-hook-form com zod para formulário de cadastro de clientes

--- src/app/dashboard/customer/page.tsx
+++ src/app/dashboard/customer/page.tsx
@@ -2,6 +2,8 @@ import { Container } from "@/components/container";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import { redirect } from "next/navigation";
+import Link from "next/link";
+import { CardCustomer } from "./components/card";
 
 export default async function Customer() {
 
@@ -12,10 +14,19 @@ export default async function Customer() {
 
     return (
         <Container>
-            <main>
-                <div>
-                    <h1>Página Customer</h1>
+            <main className="mt-9 mb-2">
+                <div className="flex items-center justify-between">
+                    <h1 className="text-3xl font-bold">Meus clientes</h1>
+                    <Link href="/dashboard/customer/new" className="bg-blue-500 px-4 py-1 rounded text-white">
+                        Novo cliente
+                    </Link>
                 </div>
+
+                <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 mt-2">
+                    <CardCustomer />
+                    <CardCustomer />
+                    <CardCustomer />
+                </section>
             </main>
         </Container>
     );
