557bc73a157e28b72adc2eae117e46629524debc
natanlimaz <natanlimadev@gmail.com>
2024-10-16 12:47:18 -0300

    feat: Criando estrutura e contexto do modal dos chamados/tickets

---
+++ src/components/modal/index.tsx
@@ -0,0 +1,15 @@
+"use client"
+export function ModalTicket() {
+
+    return (
+        <section className="absolute w-full min-h-screen bg-gray-900/80">
+            <div className="absolute inset-0 flex items-center justify-center">
+
+                <div className="bg-white shadow-lg w-4/5 md:2-1/2 max-w-2xl p-3 rounded">
+                    <h1>DETALHES DO CHAMADO</h1>
+                </div>
+
+            </div>
+        </section>
+    );
+}
