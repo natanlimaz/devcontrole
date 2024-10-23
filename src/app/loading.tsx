58fa8689318f48272e4a5248bdcf80a94ac149c8
natanlimaz <natanlimadev@gmail.com>
2024-10-22 21:32:58 -0300

    feat: Implementando loading antes de renderizar as telas

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+
+export default function Loading() {
+
+    return (
+        <div className="w-full h-screen animate-pulse flex items-center justify-center">
+            <h1 className="font-bold text-5xl pl-1 hover:tracking-widest duration-300 animate-bounce">
+                <span className="text-blue-500">DEV</span> CONTROLE
+            </h1>
+        </div>
+    );
+}
